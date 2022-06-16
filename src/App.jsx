a6468858445da415752b8afa81c00e916defdf96
Eugenia <eugeniasaf@gmail.com>
2022-06-16 22:26:06 +0200

    my solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux';
+import Home from './pages/home-page/home-page';
+import Header from './components/Header/Header';
+import WishPage from './pages/wish-page/wish-page';
+import SignupForm from './pages/autorization/autorization';
+import ContactUs from './pages/contact-me/contact-me';
+
 import './App.scss';
 
-export const App = () => (
-  <div className="starter">
-    React starter pack
-  </div>
-);
+export function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="contact" element={<ContactUs />} />
+          <Route path="wish" element={<WishPage />} />
+          <Route path="login" element={<SignupForm />} />
+        </Routes>
+      </div>
+    </Provider>
+  );
+}
